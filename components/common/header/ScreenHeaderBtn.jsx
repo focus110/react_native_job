e0761e9845a1410d175342074d16404a9d3c2c95
focus110 <iamemmaochuko@gmail.com>
2023-11-20 06:53:34 +0100

    added welcome text and search form

--- components/common/header/ScreenHeaderBtn.jsx
+++ components/common/header/ScreenHeaderBtn.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { View, Text } from "react-native";
+import { TouchableOpacity, Image } from "react-native";
 
-import styles from './screenheader.style'
+import styles from "./screenheader.style";
 
-const ScreenHeaderBtn = () => {
+const ScreenHeaderBtn = ({ iconUrl, dimension, hardlePress }) => {
   return (
-    <View>
-      <Text>ScreenHeaderBtn</Text>
-    </View>
-  )
-}
+    <TouchableOpacity>
+      <Image
+        source={iconUrl}
+        resizeMode="cover"
+        style={styles.btnImg(dimension)}
+      />
+    </TouchableOpacity>
+  );
+};
 
-export default ScreenHeaderBtn
+export default ScreenHeaderBtn;
